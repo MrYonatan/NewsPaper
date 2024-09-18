536a5008fb89f02b979cfeba017ea1fa54bc08f6
MrYonatan <yonatanassefa60@gmail.com>
2024-09-18 13:34:28 +0300

    back back to first

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,12 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    
-    
     #3rd party
     'crispy_forms',
     "crispy_bootstrap5",
-    
     
     #local
     'users.apps.UsersConfig',
